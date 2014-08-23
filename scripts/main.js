365693134ce2e5fcc92d9e5afb09eed5aa665c12
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-08-23 12:50:37 -0400

    changes planets to grey, no more 'sides'

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var planets = {};
 var planetIds = [];
-var planetsPerSide = 10;
+var totalPlanets = 10;
 var levelWidth = $(window).width()-100;
 var levelHeight = $(window).height()-100;
 var viewport = $('#viewport');
@@ -8,21 +8,21 @@ var selectedPlanet;
 
 function setupLevel() {
   currentId = 0;
-  for(var i=0; i<planetsPerSide; i++) {
+  for(var i=0; i<totalPlanets; i++) {
     var randomX = ~~(levelWidth/2)*Math.random();
     var randomY = ~~(levelHeight)*Math.random();
     var mirrorX = levelWidth-randomX;
     var mirrorY = levelHeight-randomY;
     var planetId = 'planet' + currentId;
-    planets[planetId] = {side:'red', x:randomX, y:randomY, id:planetId, selected:false};
+    planets[planetId] = {color:'grey', x:randomX, y:randomY, id:planetId, selected:false};
     planetIds.push(planetId);
-    viewport.append('<div class="planet red" id="'+planetId+'"></div>');
+    viewport.append('<div class="planet grey" id="'+planetId+'"></div>');
     currentId++;
 
     planetId = 'planet' + currentId;
-    planets[planetId] = {side:'blue', x:mirrorX, y:mirrorY, id:planetId, selected:false};
+    planets[planetId] = {color:'grey', x:mirrorX, y:mirrorY, id:planetId, selected:false};
     planetIds.push(planetId);
-    viewport.append('<div class="planet blue" id="'+planetId+'"></div>');
+    viewport.append('<div class="planet grey" id="'+planetId+'"></div>');
     currentId++;
   }
 
