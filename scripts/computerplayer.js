93cdc356a9541758532e671e01dc736a86592ac5
Maximillian von Briesen <mobyvb@gmail.com>
2014-08-25 00:31:15 -0400

    add ai to make random connections

--- scripts/computerplayer.js
+++ scripts/computerplayer.js
@@ -12,5 +12,29 @@ var ai = {
   },
 
   updateConnections: function() {
+    // finds all unconnected red planets, selects a random one, and creates a new connection to a random planet
+    var redPlanets = [];
+    var connectedRedPlanets = [];
+    var unpopulatedPlanets = [];
+
+    connectedPlanets.forEach(function(connection) {
+      if(redPlanets.indexOf(connection.from)!==-1) {
+        connectedRedPlanets.push(connection.from);
+      }
+    });
+
+    var unconnectedRedPlanets = [];
+    planetIds.forEach(function(planetId) {
+      var planet = planets[planetId];
+      if(planet.color==='red' && connectedRedPlanets.indexOf(planetId)===-1) {
+        unconnectedRedPlanets.push(planetId);
+      }
+    });
+    var newConnectedPlanet = unconnectedRedPlanets[~~(Math.random()*unconnectedRedPlanets.length)];
+    var randomPlanet = planetIds[~~(Math.random()*planetIds.length)];
+    if(newConnectedPlanet && randomPlanet && newConnectedPlanet!==randomPlanet) {
+      planetClicked($('#'+newConnectedPlanet), 1);
+      planetClicked($('#'+randomPlanet), 1);
+    }
   }
 }
