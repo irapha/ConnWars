0b524dc793ec88af39506c15b9f529f026d57e75
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-08-25 10:44:30 -0400

    Fixes huge transfer bug. Makes transfer rate slower

--- scripts/population.js
+++ scripts/population.js
@@ -12,8 +12,8 @@ function getPlanetPopulations(giver, receiver){
 
   distance = Math.sqrt(distance_x_sq + distance_y_sq);
 
-  var numTransferred = Math.atan( distance*-1 - giver.population);
-  
+  var numTransferred = (0.03)*(-1)*Math.atan(distance*(-1) - giver.population);
+
   var giverPopulation = giver.population-numTransferred;
 
   var receiverPopulation;
